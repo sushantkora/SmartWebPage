1824c9522f956758a3c0db8338ce3bae415c4392
unknown <sushant.dhb1101@gmail.com>
2021-11-18 23:15:29 +0530

    Adding delta changes

---
+++ SmartWebApp/WebApplication1/Controllers/MoviesController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using SmartWebApp.Models;
+
+namespace SmartWebApp.Controllers
+{
+    public class MoviesController : Controller
+    {
+        // GET: Movies
+        public ActionResult Random()
+        {
+            var movie = new Movies() { Title ="Intesteller",Id=1,Date =DateTime.Now,Count=1};
+            
+            return View(movie);
+        }
+    }
+}
